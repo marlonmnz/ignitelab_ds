6e2993e744f768efa0e63b14e0e25e8d0214d4a4
Marlon Menezes <marlon_menezes@outlook.com>
2022-10-13 15:37:35 -0300

    documentação do Storybook finalizada

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,15 +14,19 @@ module.exports = {
         '2xl': 32,
       },
       colors: {        
-        'black': '#000000',
-        'cyan-300': '#9BE1FB',
-        'cyan-500': '#81D8F7',
-        'gray-100': '#E1E1E6',
-        'gray-200': '#C4C4CC',
-        'gray-400': '#7C7C8A',
-        'gray-800': '#202024',
-        'gray-900': '#121214',
-        'white': '#ffffff',
+        black: '#000000',
+        cyan: {
+          300: '#9BE1FB',
+          500: '#81D8F7',
+        },
+        gray:{
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          400: '#7C7C8A',
+          800: '#202024',
+          900: '#121214',
+        },
+        white: '#ffffff',
       },
       fontFamily: {
         sans: 'Inter, sans-serif'
